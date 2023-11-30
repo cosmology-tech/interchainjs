6fc92d29f69312fdd37aaad4b4c177ecc02fd3be
liujun93 <ljun8575@gmail.com>
2023-11-30 18:13:09 +0800

    remove docs

--- packages/docs/theme.config.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default {
-  logo: <span>Sign Documentation</span>,
-  project: {
-    link: "https://github.com/cosmology-tech/sign",
-  },
-  // ... other theme options
-};
