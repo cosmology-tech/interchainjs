f30b63c60a62317995f22dd47380ffccadd2b068
Eason Smith <eason@smi.th>
2024-05-19 23:37:42 +0800

    Injective e2e test with injective testnet(WIP)

---
+++ networks/injective/starship/__tests__/testnet.test.ts
@@ -0,0 +1,47 @@
+import { DirectSecp256k1Wallet } from "@cosmjs/proto-signing";
+import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
+import { fromHex } from "@cosmjs/encoding";
+
+import { config } from 'dotenv';
+config({ path: '.env.development' });
+
+describe("Injective Chain transfer test", () => {
+  let client: SigningCosmWasmClient;
+  let fromAddress: string;
+  const toAddress = 'inj1et085dzp9xkrzfx3c75u405u4hk8d0tyfgjxqc' // receive address should exist on chain
+  const privkeyUint8Array = fromHex(process.env.TEST_PRIVATE_KEY)
+
+  beforeAll(async () => {
+    console.log('process.env.TEST_PRIVATE_KEY', process.env.TEST_PRIVATE_KEY)
+    const fromWallet = await DirectSecp256k1Wallet.fromKey(privkeyUint8Array, 'inj');
+
+    fromAddress = (await fromWallet.getAccounts())[0].address;
+
+    client = await SigningCosmWasmClient.connectWithSigner(
+      'https://testnet.sentry.tm.injective.network',
+      fromWallet
+    )
+  });
+
+  test("transfer should be successful", async () => {
+    const initialBalance = BigInt((await client.getBalance(toAddress, "uinj")).amount);
+
+    const sendAmount = '1';
+    const sendResult = await client.sendTokens(
+      fromAddress,
+      toAddress, 
+      [{denom: 'inj', amount: sendAmount}],
+      {amount: [{denom: 'inj', amount: '0' }], gas: '20000'}
+    );
+
+    expect(sendResult.code).toBeUndefined();
+
+    console.log('Waiting for 20s for transaction to be processed...');
+    await new Promise(resolve => setTimeout(resolve, 20_000));
+
+    const finalBalance = BigInt((await client.getBalance(toAddress, "uinj")).amount);
+
+    const increase = finalBalance - initialBalance;
+    expect(increase).toBe(sendAmount);
+  });
+});
