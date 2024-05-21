b468928984e43e00213bc5f6b8ae7802c96ba63f
Eason Smith <eason@smi.th>
2024-05-22 02:35:33 +0800

    get injective address from private key

--- networks/injective/starship/__tests__/testnet.test.ts
+++ networks/injective/starship/__tests__/testnet.test.ts
@@ -4,32 +4,29 @@ import { getSigningInjectiveClient } from "../../codegen-injective"
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { MsgSend, MsgBroadcasterWithPk } from '@injectivelabs/sdk-ts'
 import { Network } from '@injectivelabs/networks'
+import * as ethUtil from 'ethereumjs-util';
 
 import { config } from 'dotenv';
 config({ path: '.env.development' });
 
+const { bech32 } = require('bech32')
+
 describe("Injective Chain transfer test", () => {
-  
   let client: SigningStargateClient;
   let fromAddress: string;
   const toAddress = 'inj1et085dzp9xkrzfx3c75u405u4hk8d0tyfgjxqc' // receive address should exist on chain
   const privkeyUint8Array = fromHex(process.env.TEST_PRIVATE_KEY)
-  
   const rpcEndpoint = 'https://testnet.sentry.tm.injective.network'
-
-  let fromWallet: DirectSecp256k1Wallet
-
+  let signer: DirectSecp256k1Wallet
   beforeAll(async () => {
-    // console.log('process.env.TEST_PRIVATE_KEY', process.env.TEST_PRIVATE_KEY)
-    fromWallet = await DirectSecp256k1Wallet.fromKey(privkeyUint8Array, 'inj');
+    signer = await DirectSecp256k1Wallet.fromKey(privkeyUint8Array, 'inj');
 
-    fromAddress = 'inj10a64save5u2rpdru50x5kvwe7uyqfmshl9vwy2' // (await fromWallet.getAccounts())[0].address;
+    const privateKey = Buffer.from(process.env.TEST_PRIVATE_KEY, 'hex');
+    const addressBuffer = ethUtil.privateToAddress(privateKey);
+    fromAddress = bech32.encode('inj', bech32.toWords(addressBuffer)) // (await fromWallet.getAccounts())[0].address is different from this
     console.log('fromAddress', fromAddress)
 
-    client = await getSigningInjectiveClient({
-      rpcEndpoint,
-      signer: fromWallet
-    })
+    client = await getSigningInjectiveClient({rpcEndpoint, signer})
   });
 
   test("transfer should be successful", async () => {
@@ -52,5 +49,5 @@ describe("Injective Chain transfer test", () => {
     const finalBalance = BigInt((await client.getBalance(toAddress, "inj")).amount);
     const increase = finalBalance - initialBalance
     expect(increase).toBe( BigInt(sendAmount) );
-  }, 60_000);
+  }, 20_000);
 });
