31c5cf9bd9512ddd2ea15e2e70a24f52b91353d8
Eason Smith <eason@smi.th>
2024-06-19 16:31:12 +0800

    asserts for check tx

--- networks/cosmos/src/utils/asserts.ts
+++ networks/cosmos/src/utils/asserts.ts
@@ -1,7 +1,31 @@
 import { BroadcastResponse } from '../types';
 
+export function isCheckTxFailure(resp: BroadcastResponse) {
+  return resp.check_tx?.code !== 0
+}
+
+export function isCheckTxSuccess(resp: BroadcastResponse) {
+  return !isCheckTxFailure(resp);
+}
+
+export function assertIsCheckTxSuccess(resp: BroadcastResponse) {
+  if (isCheckTxFailure(resp)) {
+    throw new Error(
+      `Error when broadcasting tx ${resp.hash}. Code: ${resp.check_tx?.code}; Raw log: ${resp.check_tx?.log}`
+    );
+  }
+}
+
+export function assertIsCheckTxFailure(resp: BroadcastResponse) {
+  if (isCheckTxSuccess(resp)) {
+    throw new Error(
+      `Transaction ${resp.hash} did not fail. Code: ${resp.check_tx?.code}; Raw log: ${resp.check_tx?.log}`
+    );
+  }
+}
+
 export function isDeliverTxFailure(resp: BroadcastResponse) {
-  return resp.deliver_tx?.code !== 0 || resp.check_tx?.code !== 0
+  return resp.deliver_tx?.code !== 0
 }
 
 export function isDeliverTxSuccess(resp: BroadcastResponse) {
