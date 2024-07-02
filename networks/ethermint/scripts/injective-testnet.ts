9027e433bf7a054998a149d5f85a0fdada49cc92
Eason Smith <eason@smi.th>
2024-07-02 14:34:13 +0800

    injective testnet

---
+++ networks/ethermint/scripts/injective-testnet.ts
@@ -0,0 +1,25 @@
+import { MsgSend, MsgBroadcasterWithPk } from '@injectivelabs/sdk-ts'
+import { Network } from '@injectivelabs/networks'
+
+const privateKey = 'b6627188cc31f4dd3e36445e1e77fe3e1ab085f774ab264ccbc2dd767694b993'
+const injectiveAddress = 'inj10a64save5u2rpdru50x5kvwe7uyqfmshl9vwy2'
+const amount = {
+  denom: 'inj',
+  amount: '1',
+}
+const msg = MsgSend.fromJSON({
+  amount,
+  srcInjectiveAddress: injectiveAddress,
+  dstInjectiveAddress: injectiveAddress,
+});
+
+(async ()=>{
+  const txResponse = await new MsgBroadcasterWithPk({
+    privateKey,
+    network: Network.Testnet
+  }).broadcast({
+    msgs: msg
+  })
+
+  console.log('txResponse', txResponse)
+})()
